4f01f909458f13036c37dc30ef24d36019295ff3
MarceloFerreira11 <mpferreira7@gmail.com>
2023-10-11 12:52:56 +0100

    add 5 round game function

--- javas.js
+++ javas.js
@@ -12,11 +12,9 @@ function getComputerChoice() {
     }
 }
 //Play a single round player vs computer and return a string that declares the winner 
-const playerSelection = 'pAPer';
-const computerSelection = getComputerChoice();
-
+let computerSelection = getComputerChoice();
 function playRound(playerSelection,computerSelection) {
-    let roundResult = ''
+    let roundResult = '';
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     
@@ -28,26 +26,39 @@ function playRound(playerSelection,computerSelection) {
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection=== 'paper') 
     ) {
-        return roundResult = 'Player wins!'
+        return roundResult = 'Player wins!';
     } 
     else {
         return roundResult = 'Bot wins'
-    }
+    } 
 }
 
+ function game() {
+    let playerScore = 0;
+    let computerScore = 0; 
+    let message=''
+    for (let i=1; i<=5;i++) {
+        
+        let playerSelection = prompt("Round : Rock Paper or scissors?");
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection,computerSelection);
+        
+        
+        if (roundResult === ('Player wins!')) {
+        playerScore++;
+        } else if (roundResult === ('Bot wins')){
+        computerScore++;
+        }
+        console.log(`Round ${i}: \nComputer chose ${computerSelection} \n${roundResult}`)
+    }
+        message = (playerScore > computerScore) ? 'You win!' :
+        (playerScore < computerScore) ? 'You lose!' :
+        'Draw!';
+        
+          return `\nEnd result:
+          \nYour score: ${playerScore} \nComputer score: ${computerScore}
+          \n${message}`;
+ }
+          console.log(game())
 
-
- console.log(playRound (playerSelection,computerSelection))
-
- 
-
-
-
-
-
-
-
-
-
-
-   
+//new function called game() to play 5 round game that keeps score and reports a winner
